0b0775e4209c009788eb0431f71558823a19d690
Anindya <ganpachirokamaboko@gmail.com>
2021-05-28 23:28:18 +0600

    fixed too long field input error

--- fun/snipe.js
+++ fun/snipe.js
@@ -8,11 +8,13 @@ function snipe(client, Discord, message, deletedMessages) {
     if(!+deletedMessageNumber || deletedMessageNumber > 10 || deletedMessageNumber < 1) return message.reply('make sure to provide an integer number from 1 to 10~');
     if(!deletedMessagesForThisGuild || deletedMessagesForThisGuild.length < deletedMessageNumber) return message.reply('couldn\'t find that deleted message for this server, the bot must\'ve restarted and lost the data... :smiling_face_with_tear:');
 
+    const responsibleMember = deletedMessagesForThisGuild[deletedMessagesForThisGuild.length - deletedMessageNumber].member;
+    const deletedMessage = deletedMessagesForThisGuild[deletedMessagesForThisGuild.length - deletedMessageNumber].message;
     const snipeEmbed = new Discord.MessageEmbed()
         .setColor(getRandomHexColor())
         .setTitle('Got exposed b- :smiling_face_with_tear:')
-        .addField('Member', deletedMessagesForThisGuild[deletedMessageNumber - 1].member)
-        .addField('Message', deletedMessagesForThisGuild[deletedMessageNumber - 1].message)
+        .addField('Member', responsibleMember)
+        .addField('Message', (deletedMessage.length <= 1000 ? deletedMessage : `${ deletedMessage.slice(0, 1000) }...`) || '*Cannot snipe some types of messages yet, will add the feature soon ;)*')
         .setFooter('Time', client.user.displayAvatarURL())
         .setTimestamp();
 
