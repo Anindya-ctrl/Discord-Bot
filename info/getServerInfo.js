e2a2bd6d4a35f6f7f52433a485c9e419a2d23061
Anindya <ganpachirokamaboko@gmail.com>
2021-06-12 23:11:45 +0600

    fixed some  minor issues for server infos

--- info/getServerInfo.js
+++ info/getServerInfo.js
@@ -4,8 +4,11 @@ const getRandomHexColor = require("../functions/getRandomHexColor");
 function getServerInfo(client, message) {
         const { guild, author } = message;
         const { name, owner, region, memberCount, createdAt, emojis, roles, afkTimeout } = guild;
-        const guildEmojis = emojis.cache.map(emoji => `${ emoji }`).join(', ');
-        const guildRoles = roles.cache.map(role => `${ role }`).join(', ');
+
+        const guildEmojis = emojis.cache.map(emoji => `${ emoji }`);
+        const joinedGuildEmojis = guildEmojis.join(', ');
+        const guildRoles = roles.cache.map(role => `${ role }`);
+        const joinedGuildRoles = guildRoles.join(', ');
 
         const InfoEmbed = new MessageEmbed()
             .setTitle('**Server Info :face_with_monocle:**')
@@ -17,8 +20,8 @@ function getServerInfo(client, message) {
             .addField('Member count', memberCount, true)
             .addField('Created on', createdAt, true)
             .addField('AFK timeout', `${ afkTimeout / 60 }min`, true)
-            .addField(`Emojis(${ guildEmojis.split(', ').length })`, guildEmojis.length && guildEmojis.length <= 500 ? guildEmojis : guildEmojis.length > 500 ? `${ guildEmojis.slice(0, 500) }...` : 'N/A')
-            .addField(`Roles(${ guildRoles.split(', ').length })`, guildRoles.length && guildRoles.length <= 500 ? guildRoles : guildRoles.length > 500 ? `${ guildRoles.slice(0, 500) }...` : 'N/A')
+            .addField(`Emojis(${ guildEmojis.length })`, guildEmojis.length && guildEmojis.length <= 10 ? joinedGuildEmojis : guildEmojis.length > 10 ? `${ guildEmojis.slice(0, 10) }... and ${ guildEmojis.length - 10 } more.` : 'N/A')
+            .addField(`Roles(${ guildRoles.length })`, guildRoles.length && guildRoles.length <= 10 ? joinedGuildRoles : guildRoles.length > 10 ? `${ guildRoles.slice(0, 10) }... and ${ guildRoles.length - 10 } more.` : 'N/A')
             .addField('Requested by', author)
             .setFooter('Time', client.user.displayAvatarURL())
             .setTimestamp();
