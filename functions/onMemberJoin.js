a6237ff967f5edbe8bf728a213800231ad3c27c3
Anindya <ganpachirokamaboko@gmail.com>
2021-06-24 21:22:58 +0600

    don't send incomplete canvas welcome

--- functions/onMemberJoin.js
+++ functions/onMemberJoin.js
@@ -31,29 +31,31 @@ async function onMemberJoin(member) {
     const [ cachedChannelId, cachedWelcomeMessage ] = welcomeDataForThisGuild;
     const channel = channels.cache.get(cachedChannelId);
 
-    const canvas = Canvas.createCanvas(1920, 1080);
-    const canvasContext = canvas.getContext('2d');
+    // const canvas = Canvas.createCanvas(1920, 1080);
+    // const canvasContext = canvas.getContext('2d');
     
-    const backgroundImage = await Canvas.loadImage('https://cdn.discordapp.com/attachments/810415154755665951/852908063433556029/file.jpg');
-    const userPfp = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'png' }));
+    // const backgroundImage = await Canvas.loadImage('https://cdn.discordapp.com/attachments/810415154755665951/852908063433556029/file.jpg');
+    // const userPfp = await Canvas.loadImage(member.user.displayAvatarURL({ format: 'png' }));
 
-    let xAxis = 0;
-    let yAxis = 0;
-    canvasContext.drawImage(backgroundImage, xAxis, yAxis);
+    // let xAxis = 0;
+    // let yAxis = 0;
+    // canvasContext.drawImage(backgroundImage, xAxis, yAxis);
 
-    xAxis = canvas.width / 2 - userPfp.width / 2;
-    yAxis = canvas.height / 2 - userPfp.height / 2 - canvas.height / 5;
-    canvasContext.drawImage(userPfp, xAxis, yAxis, 400, 400);
+    // xAxis = canvas.width / 2 - userPfp.width / 2;
+    // yAxis = canvas.height / 2 - userPfp.height / 2 - canvas.height / 5;
+    // canvasContext.drawImage(userPfp, xAxis, yAxis, 400, 400);
 
-    canvasContext.fillStyle = '#ffffff';
-    canvasContext.font = '35px sans-serif';
-    let imageText = `Welcome Anindya~`;
+    // canvasContext.fillStyle = '#ffffff';
+    // canvasContext.font = '35px sans-serif';
+    // let imageText = `Welcome Anindya~`;
 
-    xAxis = canvas.width / 2 - canvasContext.measureText(imageText).width / 2;
-    canvasContext.fillText(imageText, xAxis, 60 + userPfp.height);
+    // xAxis = canvas.width / 2 - canvasContext.measureText(imageText).width / 2;
+    // canvasContext.fillText(imageText, xAxis, 60 + userPfp.height);
 
-    const canvasAttachment = new MessageAttachment(canvas.toBuffer());
-    channel.send(cachedWelcomeMessage.replace(/<@>/g, `<@${ memberId }>`), canvasAttachment);
+    // const canvasAttachment = new MessageAttachment(canvas.toBuffer());
+    // channel.send(cachedWelcomeMessage.replace(/<@>/g, `<@${ memberId }>`), canvasAttachment);
+
+    channel.send(cachedWelcomeMessage.replace(/<@>/g, `<@${ memberId }>`));
 }
 
 module.exports = {
